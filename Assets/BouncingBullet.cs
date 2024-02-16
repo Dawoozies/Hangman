f79914629bd632de87edf66316b6524f82dc7198
Dawoozies <davidchriswiggins@gmail.com>
2024-02-16 14:47:57 +1100

    Bouncing bullet kinda works
    Bouncing bullet kinda works

--- Assets/BouncingBullet.cs
+++ Assets/BouncingBullet.cs
@@ -6,26 +6,37 @@ public class BouncingBullet : MonoBehaviour
 {
     public float moveSpeed;
     public Vector2 velocity;
+    public Vector2 startVelocity;
     Rigidbody2D rb;
+    Vector2 lastVelocity;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        ResetBullet(Vector2.zero);
+    }
+    void Update()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+        if(rb.velocity.magnitude > 0)
+            transform.up = rb.velocity;
+        if(rb.velocity.magnitude < startVelocity.magnitude)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity*100000f, startVelocity.magnitude);
+        }
     }
     public void ResetBullet(Vector2 spawnPosition)
     {
+        if(rb == null)
+            rb = GetComponent<Rigidbody2D>();
         transform.position = spawnPosition;
         Vector2 closestEnemyPos = EnemySpawnManager.GetClosestEnemyPosition(spawnPosition);
-        Vector2 bulletDir = closestEnemyPos - spawnPosition;
+        //Vector2 bulletDir = closestEnemyPos - spawnPosition;
+        Vector2 bulletDir = startVelocity;
         transform.up = bulletDir;
         rb.velocity = bulletDir * moveSpeed;
     }
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Player")
-        {
-            return;
-        }
-        ContactPoint2D contactPoint = col.GetContact(0);
-        Vector2 reflectedVelocity = Vector2.Reflect(rb.velocity, contactPoint.normal);
+
     }
 }
